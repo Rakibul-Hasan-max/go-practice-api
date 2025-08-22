e431a93b1b85e3a9e2f9aebb49a570964c330632
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-08-22 19:18:54 +0600

    get post created

--- main.go
+++ main.go
@@ -1,7 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"imageUrl"`
+}
+
+var productsList []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	sendData(w, productsList, 200)
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	var newProduct Product
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newProduct)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "plz give me valid json", 400)
+		return
+	}
+
+	newProduct.ID = len(productsList) + 1
+
+	productsList = append(productsList, newProduct)
+
+	sendData(w, newProduct, 201)
+}
+
+func sendData(w http.ResponseWriter, data interface{}, statusCode int) {
+	w.WriteHeader(statusCode)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(data)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	mux := http.NewServeMux() //router
+
+	mux.Handle("GET /products", http.HandlerFunc(getProducts))
+	mux.Handle("POST /createProducts", http.HandlerFunc(createProduct))
+
+	fmt.Println("Server is running on port: 8080")
+
+	globalRouter := globalRouter(mux)
+
+	err := http.ListenAndServe(":8080", globalRouter) //start server
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func init() {
+	prd1 := Product{
+		ID:          1,
+		Title:       "Orange",
+		Description: "Orange is a fruit. I love it.",
+		Price:       100,
+		ImgUrl:      "https://desime.co.uk/cdn/shop/files/orange.jpg?v=1690113703",
+	}
+
+	prd2 := Product{
+		ID:          2,
+		Title:       "Apple",
+		Description: "Apple is a fruit. I love it.",
+		Price:       150,
+		ImgUrl:      "https://desime.co.uk/cdn/shop/files/orange.jpg?v=1690113703",
+	}
+
+	prd3 := Product{
+		ID:          3,
+		Title:       "Banana",
+		Description: "Banana is a fruit. I love it.",
+		Price:       200,
+		ImgUrl:      "https://desime.co.uk/cdn/shop/files/orange.jpg?v=1690113703",
+	}
+
+	productsList = append(productsList, prd1)
+	productsList = append(productsList, prd2)
+	productsList = append(productsList, prd3)
+
+}
+
+func globalRouter(mux *http.ServeMux) http.Handler {
+	handleAllReq := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Hasan")
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return
+		}
+		mux.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(handleAllReq)
 }
