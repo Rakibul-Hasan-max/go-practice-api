9c226e8258ecc3dd74c6dbb9e8369401e284dbac
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-08-26 12:37:55 +0600

    log check

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"go-practice-api/global_router"
 	"go-practice-api/handlers"
+	"go-practice-api/middleware"
 	"net/http"
 )
 
 func Serve() {
 	mux := http.NewServeMux() //router
 
-	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProducts))
-	mux.Handle("POST /products", http.HandlerFunc(handlers.CreateProduct))
-	mux.Handle("GET /products/{productID}", http.HandlerFunc(handlers.GetProductByID))
+	mux.Handle("GET /hasan", middleware.Logger(http.HandlerFunc(handlers.Test))) //log
+
+	mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handlers.GetProducts)))
+	mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handlers.CreateProduct)))
+	mux.Handle("GET /products/{productID}", middleware.Logger(http.HandlerFunc(handlers.GetProductByID)))
 
 	globalRouter := global_router.GlobalRouter(mux)
 
