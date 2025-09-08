3c9c7bd19ce29b4c7885e3e95ed516f92d3ceb70
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-09-09 00:18:03 +0600

    jwt-test

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"go-practice-api/cmd"
+	"fmt"
+	"go-practice-api/utilities"
 )
 
 func main() {
-	cmd.Serve()
+	// cmd.Serve()
+	jwt, err := utilities.CreateJwt("my-secret", utilities.Payload{
+		Sub:         35,
+		FirstName:   "Rakibul",
+		LastName:    "Hasan",
+		Email:       "hasan@example.com",
+		IsShopOwner: false,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(jwt)
 }
