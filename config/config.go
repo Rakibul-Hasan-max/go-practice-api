92dffb2c153809e9d3220ba3970c32a87b02e520
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-10-02 22:45:38 +0600

    db-config-env

--- config/config.go
+++ config/config.go
@@ -10,11 +10,21 @@ import (
 
 var configurations *Config
 
+type DBConfig struct {
+	Host          string
+	Port          int
+	Name          string
+	User          string
+	Password      string
+	EnableSSLMODE bool
+}
+
 type Config struct {
 	Version      string
 	ServiceName  string
 	HttpPort     int
 	JwtSecretKey string
+	DB           *DBConfig
 }
 
 func loadConfig() {
@@ -56,11 +66,65 @@ func loadConfig() {
 		os.Exit(1)
 	}
 
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		fmt.Println("DB Host is required")
+		os.Exit(1)
+	}
+
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		fmt.Println("DB port is required")
+		os.Exit(1)
+	}
+
+	dbPrt, err := strconv.ParseInt(dbPort, 10, 64)
+	if err != nil {
+		fmt.Println("DB Port must be a number")
+		os.Exit(1)
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		fmt.Println("DB Name is required")
+		os.Exit(1)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		fmt.Println("DB User is required")
+		os.Exit(1)
+	}
+
+	dbPass := os.Getenv("DB_PASSWORD")
+	if dbPass == "" {
+		fmt.Println("DB password is required")
+		os.Exit(1)
+	}
+
+	enableSslMode := os.Getenv("DB_ENABLE_SSL_MODE")
+
+	enblSSLMode, err := strconv.ParseBool(enableSslMode)
+	if err != nil {
+		fmt.Println("Invalid enable ssl mode value", err)
+		os.Exit(1)
+	}
+
+	dbConfig := &DBConfig{
+		Host:          dbHost,
+		Port:          int(dbPrt),
+		Name:          dbName,
+		User:          dbUser,
+		Password:      dbPass,
+		EnableSSLMODE: enblSSLMode,
+	}
+
 	configurations = &Config{
 		Version:      version,
 		ServiceName:  serviceName,
 		HttpPort:     int(port), // type casting int64 to int
 		JwtSecretKey: jwtSecretKey,
+		DB:           dbConfig,
 	}
 }
 
