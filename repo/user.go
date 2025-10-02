9225646defdb6adaea5c9d91991053a6b97a9e38
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-10-03 00:13:31 +0600

    db-migration

--- repo/user.go
+++ repo/user.go
@@ -34,7 +34,7 @@ func NewUserRepo(db *sqlx.DB) UserRepo {
 	}
 }
 
-func (r userRepo) Create(user User) (*User, error) {
+func (r *userRepo) Create(user User) (*User, error) {
 	// Insert query with RETURNING id
 	query := `
 		INSERT INTO users (
